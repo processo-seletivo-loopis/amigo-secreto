cdf0915f7c371b3798b16436a6ac3b64e3534e0b
ivesfg1 <wigzfurtado@gmail.com>
2021-06-19 03:11:47 -0300

    create: "UserApp" model on django_app

--- backend/django_app/admin.py
+++ backend/django_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserApp
 
-# Register your models here.
+
+class UserAppAdmin(admin.ModelAdmin):
+    list_display = ("name", "email")
+    list_display_links = ("name",)
+
+
+admin.site.register(UserApp, UserAppAdmin)
