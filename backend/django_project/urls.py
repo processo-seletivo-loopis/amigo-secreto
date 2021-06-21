f2ea4430a314739c457b09b5b1c152ed40b56511
ivesfg1 <wigzfurtado@gmail.com>
2021-06-21 15:31:23 -0300

    create: views and urls, fix: settings.py corsheaders issue

--- backend/django_project/urls.py
+++ backend/django_project/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from django_app import views
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserAppView, "user")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
